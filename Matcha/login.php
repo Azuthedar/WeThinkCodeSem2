dcf154aa696464501032494c6d578edabf91ee5d
Johann BUYS <jbuys@e5r6p5.jnb.42.fr>
2016-11-02 13:01:51 +0200

    Updated Accounts. Not Working yet jumping between certain spaces error.

---
+++ Matcha/login.php
@@ -0,0 +1,26 @@
+<?php
+
+include('connect.php');
+
+ini_set("display_startup_errors", 1);
+ini_set("display_errors", 1);
+
+$email = $_GET['email'];
+$pass = hash("whirlpool", $_GET['password']);
+
+$conn = Connect();
+$st = $conn->prepare("SELECT * FROM `users` WHERE `email` = :email AND `password` = :password");
+$st->bindParam(":email", $email);
+$st->bindParam(":password", $pass);
+$st->execute();
+$row = $st->fetch(PDO::FETCH_ASSOC);
+if ($st->rowCount() == 1)
+{
+	header("refresh:0;url=account.php?logged=$email");
+}
+else
+{
+	header("refresh:0;url=index.php?error=true");
+}
+
+?>
---
+++ Matcha/update_bio.php
@@ -0,0 +1,22 @@
+<?php
+
+$email = $_GET['logged'];
+$n_biography = $_GET['n_biography'];
+
+$conn = Connect();
+$st = $conn->prepare("SELECT * FROM `users` WHERE email = :email");
+$st->bindParam(':email', $email);
+$st->execute();
+if (st->rowCount() == 1)
+{
+	$nBio = $conn->prepare("UPDATE `users` SET `biography` = :n_biography");
+	$nBio->bindParam('n:biography', $n_biography);
+	$nBio->execute();
+	header("refresh:0;url=account.php?bio=new");
+}
+else
+{
+	header("refresh:0;url=account.php?email=invalid");
+}
+
+?>
